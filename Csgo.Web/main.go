0bbabb57e06317c17c08b7936df376ee255a4a9e
EC2 Default User <ec2-user@ip-172-31-39-162.ec2.internal>
2019-08-14 20:01:08 +0000

    commit teste

--- Csgo.Web/main.go
+++ Csgo.Web/main.go
@@ -1,6 +1,52 @@
 package main
 
-func main(){
-	router := gin.Default()
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Root struct {
+	Func []Funcionario `json:""`
 }
-	
+
+// A Response struct to map the Entire Response
+type Funcionario struct {
+	ID           int     `json:idFuncionario`
+	Name         string  `json:"name"`
+	CPF          string  `json:"cpf"`
+	DataCadastro string  `json:"dataCad"`
+	Cargo        string  `json:"cargo"`
+	UF           string  `json:"ufNasc"`
+	Salario      float64 `json:"salario"`
+	Status       string  `json:"status"`
+}
+
+func main() {
+	response, err := http.Get("https://ledacards20190524012526.azurewebsites.net/api/Home/BuscaCPF/80478555490")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	//fmt.Println(responseData)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("ERRO")
+	}
+
+	var responseObject Root
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject)
+	//fmt.Println(len(responseObject.Name))
+
+	//	for i := 0; i < len(responseObject.Name); i++ {
+	//	fmt.Println(responseObject.Name)
+	//	}
+
+}
