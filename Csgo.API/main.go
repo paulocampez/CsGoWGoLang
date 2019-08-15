afd1fdf728fc87e7690953c3837181bcd5407dee
PauloRobertoCampez <paulo.campez@midassolutions.com.br>
2019-08-14 21:15:40 -0300

    Adicionado funcoes para log do quake
    Adicionado funcoes para log do quake

--- Csgo.API/main.go
+++ Csgo.API/main.go
@@ -2,13 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strings"
 
-	"./config"
-	"./controllers"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
+
+type Deaths struct {
+	deathsFather []DeathGame
+}
+
+type DeathGame struct {
+	player1 string
+	player2 string
+}
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+)
+
+func readFile() []string {
+	dat, _ := ioutil.ReadFile(basepath + "\\logs\\games.log")
+
+	trimSpaces := strings.TrimSpace(string(dat))
+	lstLines := strings.Split(trimSpaces, "\n")
+	fmt.Println(strings.Contains(lstLines[1], "InitGame"))
+	return lstLines
+}
+
+func checkDeaths(lines []string) Deaths {
+	var deathsMajor Deaths
+	for _, element := range lines {
+		strRegex := "[ˆ0-9]: (.*) killed (.*) [ˆb,y]"
+		match, _ := regexp.MatchString(strRegex, element)
+		if match {
+
+			r, _ := regexp.Compile(strRegex)
+			nickname := r.FindStringSubmatch(element)
+			deathsMajor.deathsFather = append(deathsMajor.deathsFather, DeathGame{player1: string(nickname[1]), player2: string(nickname[2])})
+		}
+	}
+	return deathsMajor
+}
+
+func checkGamesQt() int {
+	lines := readFile()
+	qtt := 0
+	for _, element := range lines {
+		if strings.Contains(element, "InitGame") {
+			qtt++
+		}
+	}
+	fmt.Println(qtt)
+	return qtt
+}
 
 type Credential struct {
 	Username string `json:"username"`
@@ -16,17 +69,20 @@ type Credential struct {
 }
 
 func main() {
-	db := config.DBInit()
-	router := gin.Default()
-	Db := &controllers.Db{DB: db}
 
-	router.GET("/items/:id", Db.GetItemsById)
-	router.GET("/items", Db.GetItems)
-	router.POST("/items", Db.CreateItem)
-	router.PUT("/items", Db.UpdatePerson)
-	router.DELETE("/items/:id", Db.DeleteItem)
-	router.Run(":8080")
-	fmt.Println(db)
+	checkGamesQt()
+
+	// db := config.DBInit()
+	// router := gin.Default()
+	// Db := &controllers.Db{DB: db}
+
+	// router.GET("/items/:id", Db.GetItemsById)
+	// router.GET("/items", Db.GetItems)
+	// router.POST("/items", Db.CreateItem)
+	// router.PUT("/items", Db.UpdatePerson)
+	// router.DELETE("/items/:id", Db.DeleteItem)
+	// router.Run(":8080")
+	// fmt.Println(db)
 
 	//Login usando jwt
 	//router.POST("/login", LoginHandler)
