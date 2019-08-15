6483f08b2061084eda0521928f175b432f8957d9
EC2 Default User <ec2-user@ip-172-31-43-98.us-east-2.compute.internal>
2019-08-15 16:13:06 +0000

    evolucaos

--- Csgo.API/main.go
+++ Csgo.API/main.go
@@ -9,25 +9,13 @@ import (
 )
 
 func main() {
-	//fmt.Println(service.CheckDeaths(service.GetByRound(1)))
-	//fmt.Println(service.GetAllKillsInRound(21))
-	//fmt.Println(service.GetPlayersByRound(2))
 	var root = service.GetParser()
-	//fmt.Println(service.CheckDeathsByRound(service.GetByRound(1)))
-	//checkGamesQt()
-
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"result": root,
 		})
 	})
-
-	// router.GET("/items", Db.GetItems)
-	// router.POST("/items", Db.CreateItem)
-	// router.PUT("/items", Db.UpdatePerson)
-	// router.DELETE("/items/:id", Db.DeleteItem)
 	r.Run(":8080")
-	// fmt.Println(db)
 
 }
